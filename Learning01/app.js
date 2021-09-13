607ccdf8cab13ff67f7832040add86e5993f8b61
irinniada123 <irinniada123@gmail.com>
2021-09-13 16:16:41 +0300

    rewrite createServer; add TODO

--- Learning01/app.js
+++ Learning01/app.js
@@ -8,11 +8,17 @@ let irina = new User("Irina", 32);
 irina.sayHi();
 
 const http = require("http");
-http.createServer(function (request, response) {
+http.createServer((request, response) => {
 
-    response.end(`Date: ${greeting.date}` + "\n" + greeting.getMessage(userName));
+    response.write('<html>');
+    response.write('<head><title>New Form</title><head>');
+    response.write(`<body>`);
+    response.write(`<div>Date: ${greeting.date}</div>`); //TODO: fix encoding
+    response.write(`<div>${greeting.getMessage(userName)}</div>`);
+    response.write(`</body>`);
+    response.write('</html>');
+    response.end();
 }).listen(3000, "127.0.0.1", function () {
     console.log("Server starts listening in p 3000");
 })
-
 
